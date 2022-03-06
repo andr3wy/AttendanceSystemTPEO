36f30135d5ecd10a0fe8acb0d86e726b72517923
andr3wy <34847655+andr3wy@users.noreply.github.com>
2022-03-05 18:28:09 -0600

    Delete backend/firebase directory

--- backend/firebase/cred.js
+++
@@ -1,9 +0,0 @@
-const firebase = require("firebase-admin");
-const credentials = require("./cred.json");
-
-firebase.initializeApp({
-  credential: firebase.credential.cert(credentials),
-  databaseURL: "https://<yourproject>.firebaseio.com",
-});
-
-module.exports = firebase;
